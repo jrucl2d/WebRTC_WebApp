0c3d3c2f25300acc6411aff8d72892d3bd11d410
jrucl2d <ysk789465@gmail.com>
2020-11-04 14:58:19 +0900

    다대 다 용으로 1대 1은 구현

--- front/src/routes/Room.jsx
+++ front/src/routes/Room.jsx
@@ -3,17 +3,16 @@ import io from "socket.io-client";
 
 function Room({ match }) {
   const userVideo = useRef();
-
-  const partnerVideo = useRef([]);
+  const [partnerVideos, setPartnerVideos] = useState([]);
   const peerRef = useRef(); // peer 객체 생성에 사용하는 임시 변수
   const socketRef = useRef();
   const otherUsers = useRef([]); // 다른 유저들의 userID를 저장
-  const [peers, setPeers] = useState([]);
+  const peers = useRef([]);
   const userStream = useRef();
 
   useEffect(() => {
     navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
+      .getUserMedia({ video: true, audio: false })
       .then(getStream);
   }, []);
 
@@ -34,6 +33,8 @@ function Room({ match }) {
 
     // 기존 사람들 입장에서 다른 유저가 들어왔음을 확인
     socketRef.current.on("user joined", (userID) => {
+      console.log("새로운 사람이 들어왔습니다");
+      console.dir(otherUsers.current);
       otherUsers.current.push(userID);
     });
     socketRef.current.on("offer", handleRecieveCall); // Callee는 Caller의 offer을 들을 것
@@ -47,7 +48,8 @@ function Room({ match }) {
     userStream.current // 상대방에게 offer하기 위해서 stream 정보를 peer의 track에 추가
       .getTracks()
       .forEach((track) => peerRef.current.addTrack(track, userStream.current));
-    setPeers([...peers, peerRef.current]); // 기존의 peer 배열에 추가
+    peers.current.push(peerRef.current);
+    // setPeers([...peers, peerRef.current]); // 기존의 peer 배열에 추가
   };
 
   // 나 자신의 peer 객체를 생성하는데 상대방(userID)와의 offer, answer작업에 대한 콜백 함수를 설정
@@ -74,19 +76,20 @@ function Room({ match }) {
 
   // Caller 입장에서 Offer을 제공(offer 이벤트를 emit)
   const handleNegotiationNeededEvent = (userID) => {
-    const peerIndex = otherUsers.current.findIndex(
+    const index = otherUsers.current.findIndex(
       (otherUser) => otherUser === userID
     );
-    peers[peerIndex]
+    const thePeer = peers.current[index];
+    thePeer
       .createOffer()
       .then((offer) => {
-        return peers[peerIndex].setLocalDescription(offer); // offer을 생성하고 해당 offer을 local description으로 설정
+        return thePeer.setLocalDescription(offer); // offer을 생성하고 해당 offer을 local description으로 설정
       })
       .then(() => {
         const payload = {
           target: userID,
           caller: socketRef.current.id,
-          sdp: peerRef.current.localDescription,
+          sdp: thePeer.localDescription,
         };
         socketRef.current.emit("offer", payload);
       })
@@ -96,27 +99,29 @@ function Room({ match }) {
   function handleRecieveCall(incoming) {
     peerRef.current = null;
     peerRef.current = createPeer(); // negotiate을 하는 Caller의 입장이 아니므로 상대방 userID를 보낼 필요 없음
+    peers.current.push(peerRef.current);
+    const maxNum = peers.current.length;
+    const thePeer = peers.current[maxNum - 1];
     const desc = new RTCSessionDescription(incoming.sdp);
-    peerRef.current
+    thePeer
       .setRemoteDescription(desc)
       .then(() => {
         userStream.current.getTracks().forEach(
-          (track) => peerRef.current.addTrack(track, userStream.current) // 상대방에게 나의 stream 정보를 answer하기 위해 peer에 track 정보추가
+          (track) => thePeer.addTrack(track, userStream.current) // 상대방에게 나의 stream 정보를 answer하기 위해 peer에 track 정보추가
         );
       })
       .then(() => {
-        return peerRef.current.createAnswer();
+        return thePeer.createAnswer();
       })
       .then((answer) => {
-        return peerRef.current.setLocalDescription(answer); // offer와 유사하게 sdp 정보를 가지고 있음
+        return thePeer.setLocalDescription(answer); // offer와 유사하게 sdp 정보를 가지고 있음
       })
       .then(() => {
         const payload = {
           target: incoming.caller,
           caller: socketRef.current.id,
-          sdp: peerRef.current.localDescription,
+          sdp: thePeer.localDescription,
         };
-        setPeers([...peers, peerRef.current]); // 생성한 피어 추가
         socketRef.current.emit("answer", payload);
       });
   }
@@ -127,7 +132,8 @@ function Room({ match }) {
     const index = otherUsers.current.findIndex(
       (otherUser) => otherUser === message.caller
     );
-    peers[index].setRemoteDescription(desc).catch((e) => console.log(e));
+    const thePeer = peers.current[index];
+    thePeer.setRemoteDescription(desc).catch((e) => console.log(e));
   };
 
   // Ice Candidate 정보는 서로 주고 받음
@@ -135,35 +141,50 @@ function Room({ match }) {
   const handleICECandidateEvent = (e) => {
     if (e.candidate) {
       const payload = {
-        target: otherUser.current,
         caller: socketRef.current.id,
         candidate: e.candidate,
       };
+      console.log("ICE 데이터 보냅니다.");
       socketRef.current.emit("ice-candidate", payload);
     }
   };
 
   // Ice Cnadidate 이벤트가 발생해서 상대방이 해당 정보를 전송하면, 그 정보를 받음
   const handleNewICECandidateMsg = (incoming) => {
+    console.log("ICE 데이터 받습니다.");
     const candidate = new RTCIceCandidate(incoming.candidate);
-    const index = otherUsers.findIndex(
+
+    const index = otherUsers.current.findIndex(
       (otherUser) => otherUser === incoming.caller
     );
-    peers[index].addIceCandidate(candidate).catch((e) => console.log(e));
+    const thePeer = peers.current[index];
+    thePeer
+      .addIceCandidate(candidate)
+      .catch((e) => console.log("ICE 에러" + e));
   };
 
   const handleTrackEvent = (e) => {
-    partnerVideo.current.push(e.streams[0]);
-    partnerVideo.current.srcObject = e.streams[0];
+    setPartnerVideos([...partnerVideos, e.streams[0]]);
   };
 
   return (
     <div>
       <h1>Video Chat</h1>
-      <video autoPlay ref={userVideo} />
-      <video autoPlay ref={partnerVideo} />
+      <video autoPlay width="200px" ref={userVideo} />
+      {partnerVideos.length}
+      {partnerVideos &&
+        partnerVideos.map((partnerVideo, index) => (
+          <Video key={index} stream={partnerVideo} />
+        ))}
     </div>
   );
 }
+const Video = ({ stream }) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.srcObject = stream;
+  }, []);
+  return <video width="200px" autoPlay ref={ref} />;
+};
 
 export default Room;
--- server/socket.js
+++ server/socket.js
@@ -11,10 +11,10 @@ module.exports = async (server) => {
       } else {
         rooms[roomID] = [socket.id]; // 방장은 배열에 넣어서 처음 넣어줌
       }
-      const otherUser = rooms[roomID].find((id) => id !== socket.id);
-      if (otherUser) {
-        socket.emit("other user", otherUser); // 본인에게 기존 사람이 있다고 알림
-        socket.to(otherUser).emit("user joined", socket.id); // 기존 사람에게는 본인이 새로 들어간다고 알림
+      const otherUsers = rooms[roomID].filter((id) => id !== socket.id);
+      if (otherUsers) {
+        socket.emit("other users", otherUsers); // 본인에게 기존 사람이 있다고 알림
+        socket.broadcast.emit("user joined", socket.id); // 기존 사람들에게는 본인이 새로 들어간다고 알림
       }
     });
     socket.on("offer", (payload) => {
@@ -24,7 +24,7 @@ module.exports = async (server) => {
       io.to(payload.target).emit("answer", payload);
     });
     socket.on("ice-candidate", (incoming) => {
-      io.to(incoming.target).emit("ice-candidate", incoming);
+      socket.broadcast.emit("ice-candidate", incoming);
     });
   });
 };
